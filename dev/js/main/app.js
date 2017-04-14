57a27c874cbb399c167111639acb0163f0ac68f2
Ela Yudhanira <elayudhanira@Elas-MacBook-Air.local>
2017-04-15 00:29:57 +0200

    Update style and header

--- dev/js/main/app.js
+++ dev/js/main/app.js
@@ -6,6 +6,7 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
   $routeProvider
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "HomeCtrl"})
+    .when("/home", {templateUrl: "partials/home.html", controller: "HomeCtrl"})
     .when("/howto",{templateUrl: "partials/howto.html", controller: "HomeCtrl"});
     // Pages
 
@@ -35,19 +36,19 @@ app.controller('HomeCtrl', ['$http','$scope','$location',function($http, $scope,
 
   $scope.locations=[{
     question:'Here takes place the ceremonial activity of changing those in charge of protecting the head of state.',
-    bonusPoint:'../images/09.jpg'
+    bonusPoint:'img/09.jpg'
   },{
     question:'The democratic representatives of the people congregate here, shaping the country.',
-    bonusPoint:'../images/10.jpg'
+    bonusPoint:'img/10.jpg'
   },{
     question:'Where you can see a statue of one of Sweden’s most respected musicians, Evert Taube',
-    bonusPoint:'../images/06.jpg'
+    bonusPoint:'img/06.jpg'
   },{
     question:'Where you can see the most photographed buildings in STHLM',
-    bonusPoint:'../images/07.jpg'
+    bonusPoint:'img/07.jpg'
   },{
     question:'Where you can step your feet at the very centre point of the Swedish capital',
-    bonusPoint:'../images/08.jpg'
+    bonusPoint:'img/08.jpg'
   }];
 }]);
 
@@ -61,10 +62,10 @@ app.directive('modal',function(){
       },
       template: '<div class="modal-wrapper" ng-show="showModal" ng-click="modalClicked()">'+
                 '<div class="rows">'+
-                  '<div class="col-sm-12 pull-center"><a href="" class="h4" ng-click="modalClicked()">Close</a></div>'+
                   '<div class="col-md-offset-3 col-md-6 col-sm-12">'+
                     '<img ng-src="{{modalUrl}}" class="image-modal">'+
                   '</div>'+
+                  '<div class="col-sm-12 pull-center"><a href="" class="h4" ng-click="modalClicked()">Close</a></div>'+
                 '</div>'+
               '</div>',
       link: function (scope, element, attrs) {
